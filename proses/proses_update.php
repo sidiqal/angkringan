969ab5e5befdf8df672cd638def654de8f16c2a7
inoiz32 <sidiqtamvan@gmail.com>
2020-11-15 18:07:03 +0700

    add update gambar fiture

--- proses/proses_update.php
+++ proses/proses_update.php
@@ -1,22 +1,28 @@
-<?php 
+<?php
 include("config.php");
+require("proses_upload_image.php");
 
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $id = $_POST["id"];
     $nama_makanan = $_POST["nama_makanan"];
     $harga_makanan = $_POST["harga_makanan"];
     $stok_makanan = $_POST["stok_makanan"];
+    $gambar_lama = $_POST["gambar_lama"];
 
-    $sql = "UPDATE makanan SET nama_makanan='$nama_makanan',harga_makanan='$harga_makanan',stok_makanan='$stok_makanan' WHERE id=$id";
+    if ($_FILES["gambar_makanan"]["error"] === 4) {
+        $gambar_makanan = $gambar_lama;
+    } else {
+        $gambar_makanan = upload();
+    }
+
+    $sql = "UPDATE makanan SET nama_makanan='$nama_makanan',harga_makanan='$harga_makanan',stok_makanan='$stok_makanan',gambar_makanan='$gambar_makanan' WHERE id=$id";
 
     $query = mysqli_query($conn, $sql);
 
-    if($query){
+    if ($query) {
         header("Location: ../admin/dashboard_admin.php");
         exit;
     } else {
         die("Gagal Update!");
     }
 }
-
-?>
