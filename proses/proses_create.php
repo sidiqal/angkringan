761edb2f4c276e9e86b9d27ae37b8933cc332ed6
inoiz32 <sidiqtamvan@gmail.com>
2020-11-15 17:55:12 +0700

    add upload image feature

--- proses/proses_create.php
+++ proses/proses_create.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 include("config.php");
+include("proses_upload_image.php");
 
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $nama_makanan = $_POST["nama_makanan"];
     $harga_makanan = $_POST["harga_makanan"];
     $stok_makanan = $_POST["stok_makanan"];
+    $gambar_makanan = upload();
 
-    $sql = "INSERT INTO makanan(nama_makanan, harga_makanan, stok_makanan) VALUES ('$nama_makanan', '$harga_makanan', '$stok_makanan')";
+    $sql = "INSERT INTO makanan(nama_makanan, harga_makanan, stok_makanan, gambar_makanan) VALUES ('$nama_makanan', '$harga_makanan', '$stok_makanan', '$gambar_makanan')";
 
     $query = mysqli_query($conn, $sql);
 
-    if($query) {
+    if ($query) {
         header("Location: ../admin/dashboard_admin.php");
         exit;
     } else {
         die("Gagal menambahkan!");
     }
 }
-
-?>
