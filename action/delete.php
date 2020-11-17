0e946aac67caf924a2c1fefd6404c0205abc6e94
inoiz32 <sidiqtamvan@gmail.com>
2020-11-17 20:33:38 +0700

    add session validator

--- action/delete.php
+++ action/delete.php
@@ -1,4 +1,10 @@
-<?php 
+<?php
+if ($_SESSION['level'] !== "admin") {
+    header("Location: ../index.php");
+    exit;
+}
+
+
 include("../proses/config.php");
 
 $id = $_GET["id"];
@@ -7,10 +13,8 @@ $sql = "DELETE FROM makanan WHERE id=$id";
 
 $query = mysqli_query($conn, $sql);
 
-if($query){
+if ($query) {
     header("Location: ../admin/dashboard_admin.php");
 } else {
     die("Gagal Delete!");
 }
-
-?>
