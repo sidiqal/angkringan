ac7e5c1e7a23970b3ff603c723e77924dcdef708
inoiz32 <sidiqtamvan@gmail.com>
2020-11-17 20:36:35 +0700

    change session maker

--- proses/proses_login.php
+++ proses/proses_login.php
@@ -1,24 +1,27 @@
-<?php 
+<?php
 session_start();
 include("config.php");
 
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = "SELECT * FROM user";
+    $sql = "SELECT * FROM user WHERE username='$username'";
 
     $query = mysqli_query($conn, $sql);
 
-    $data = mysqli_fetch_array($query);
+    // Cek username
+    if (mysqli_num_rows($query) === 1) {
 
-    if($password === $data["password"]){
-        $_SESSION['login'] = true;
-        header("Location: ../admin/dashboard_admin.php");
-        exit;
-    } else {
-        die("Wrong Password!");
+        // Cek password
+        $data = mysqli_fetch_array($query);
+        // cek level
+        if ($data['level'] == "admin" or $data['level'] == "pegawai") {
+            $_SESSION['username'] = $username;
+            $_SESSION['level'] = $data['level'];
+
+            header("Location: ../admin/dashboard_admin.php");
+            exit;
+        }
     }
 }
-
-?>
